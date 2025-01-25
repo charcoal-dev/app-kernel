cdac2cc96c36dfd3a062bd6cab455ddde742d04b
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-01-26 01:06:01 +0400

    Add directory validation method to Directories class
    Introduce `validateDirectory` to verify readability and writability of a directory, with detailed exception handling. This enhances robustness and ensures proper error reporting for filesystem operations.

--- src/Directories.php
+++ src/Directories.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Charcoal\App\Kernel;
 
 use Charcoal\Filesystem\Directory;
+use Charcoal\Filesystem\Exception\FilesystemException;
 
 /**
  * Class Directories
@@ -15,5 +16,29 @@ class Directories
         public readonly Directory $root
     )
     {
+    }
+
+    /**
+     * @param string $dirPath
+     * @param bool $checkReadable
+     * @param bool $checkWritable
+     * @return Directory
+     */
+    protected function validateDirectory(string $dirPath, bool $checkReadable, bool $checkWritable): Directory
+    {
+        try {
+            $dir = $this->root->getDirectory($dirPath, createIfNotExists: false);
+            if ($checkReadable && !$dir->isReadable()) {
+                throw new \RuntimeException(sprintf('Directory "%s" is not readable', $dirPath));
+            }
+
+            if ($checkWritable && !$dir->isWritable()) {
+                throw new \RuntimeException(sprintf('Directory "%s" is not writable', $dirPath));
+            }
+
+            return $dir;
+        } catch (FilesystemException $e) {
+            throw new \RuntimeException(sprintf('Directory "%s" error %s', $dirPath, $e->error->name));
+        }
     }
 }
