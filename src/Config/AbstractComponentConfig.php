a81a586065efbff7131c5704d7ef6245c7a1b0fd
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-02 17:40:33 +0400

    Add abstract class AbstractComponentConfig

---
+++ src/Config/AbstractComponentConfig.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Charcoal\App\Kernel\Config;
+
+use Charcoal\App\Kernel\Entity\AbstractEntity;
+
+/**
+ * Class AbstractComponentConfig
+ * @package Charcoal\App\Kernel\Config
+ */
+abstract class AbstractComponentConfig extends AbstractEntity
+{
+    public const ?string CONFIG_ID = null;
+
+    /**
+     * @return array
+     */
+    protected function collectSerializableData(): array
+    {
+        $data = [];
+        $reflection = new \ReflectionClass($this);
+        foreach ($reflection->getProperties(\ReflectionProperty::IS_PUBLIC | \ReflectionProperty::IS_PROTECTED) as $property) {
+            $prop = $property->name;
+            if (isset($this->$prop)) {
+                $data[$prop] = $this->$prop;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @return \class-string[]
+     */
+    public static function childClasses(): array
+    {
+        return [static::class];
+    }
+
+    /**
+     * @return string
+     */
+    final public function getPrimaryId(): string
+    {
+        if (static::CONFIG_ID === null || static::CONFIG_ID === "") {
+            throw new \LogicException(sprintf('CONFIG_ID must be defined in class "%s"', static::class));
+        }
+
+        return static::CONFIG_ID;
+    }
+}
