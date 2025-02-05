b3d0f8ee2f86a45808c9a3f98a198478cce49f96
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-05 13:35:34 +0400

    Optimize cache TTL determination in AbstractOrmRepository

--- src/Orm/AbstractOrmRepository.php
+++ src/Orm/AbstractOrmRepository.php
@@ -221,6 +221,7 @@ abstract class AbstractOrmRepository
             }
 
             try {
+                $cacheTtl = max($cacheTtl, $this->entityCacheTtl);
                 $cacheTtl = $cacheTtl > 0 ? $cacheTtl : $this->entityCacheTtl;
                 $this->module->memoryCache->storeInCache($entityId, $entity->returnCacheableObject(), $cacheTtl > 0 ? $cacheTtl : null);
                 $storedInCache = true;
