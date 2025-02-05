278004a7878e3c311abaa416abbfb79efd94541c
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-05 11:04:57 +0400

    Add utility methods to ObjectStoreEncryption enum

--- src/Contracts/ObjectStore/ObjectStoreEncryption.php
+++ src/Contracts/ObjectStore/ObjectStoreEncryption.php
@@ -12,4 +12,20 @@ enum ObjectStoreEncryption
     case DISABLED;
     case CACHE_ENCRYPTED;
     case CACHE_DECRYPTED;
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this !== self::DISABLED;
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldCacheEncrypted(): bool
+    {
+        return $this === self::CACHE_ENCRYPTED;
+    }
 }
