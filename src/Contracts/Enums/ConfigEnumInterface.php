5390cae24bb104b94d91eebef92cff2ad85581dd
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-16 01:35:29 +0500

    Add `ConfigEnumInterface` under `Contracts\Enums`

---
+++ src/Contracts/Enums/ConfigEnumInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Part of the "charcoal-dev/app-kernel" package.
+ * @link https://github.com/charcoal-dev/app-kernel
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\App\Kernel\Contracts\Enums;
+
+/**
+ * Interface ConfigEnumInterface
+ * @package Charcoal\App\Kernel\Contracts\Enums
+ */
+interface ConfigEnumInterface extends \UnitEnum
+{
+    public function getConfigKey(): string;
+}
