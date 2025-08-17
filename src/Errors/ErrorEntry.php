52ab652a8f502a1634decd876dac62199001aa57
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-17 19:29:55 +0500

    Refactor `ErrorEntry` backtrace logic to use `isDebug` and adjust `debugBacktraceOffset` call for improved environment handling.

--- src/Errors/ErrorEntry.php
+++ src/Errors/ErrorEntry.php
@@ -28,7 +28,7 @@ final readonly class ErrorEntry
     {
         $this->filePath = $errors->getRelativeFilepath($file);
         $this->levelStr = $errors->getErrorLevelStr($this->level);
-        $this->backtrace = ($errors->debugBacktraceLevel <= $this->level) ?
-            array_slice(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), $errors->backtraceOffset) : null;
+        $this->backtrace = $errors->env->isDebug() ?
+            array_slice(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), $errors->debugBacktraceOffset(null)) : null;
     }
 }
