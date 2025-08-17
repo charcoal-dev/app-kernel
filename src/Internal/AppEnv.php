fea7e763eba8dac254f36c82e37329ca9641fbf9
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-17 20:46:35 +0500

    Add `ErrorManagerConfig` and update `AppEnv` with error manager policy and debug mode methods

--- src/Internal/AppEnv.php
+++ src/Internal/AppEnv.php
@@ -8,12 +8,38 @@ declare(strict_types=1);
 
 namespace Charcoal\App\Kernel\Internal;
 
+use Charcoal\App\Kernel\Internal\Config\ErrorManagerConfig;
+
 /**
  * Class AppEnv
  * @package Charcoal\App\Kernel\Internal
+ * @internal
  */
 enum AppEnv: string
 {
     case Dev = "dev";
     case Prod = "prod";
+    case Test = "test";
+
+    /**
+     * @return bool
+     */
+    public function isDebug(): bool
+    {
+        return match ($this) {
+            self::Prod => false,
+            default => true,
+        };
+    }
+
+    /**
+     * @return ErrorManagerConfig
+     */
+    public function errorManagerPolicy(): ErrorManagerConfig
+    {
+        return new ErrorManagerConfig(
+            $this !== self::Test,
+            $this !== self::Test ? "./log/error.log" : null
+        );
+    }
 }
