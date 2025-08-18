299d075678b2c60478489c757cd6344d6f05d578
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-18 19:07:25 +0500

    Rename `LogEntryImmutable` to `LogEntryDto` and refactor properties for type consistency and improved exception handling.

--- src/Diagnostics/ExecutionSnapshot/Immutable/LogEntryDto.php
+++ src/Diagnostics/ExecutionSnapshot/Immutable/LogEntryDto.php
@@ -6,32 +6,32 @@
 
 declare(strict_types=1);
 
-namespace Charcoal\App\Kernel\Diagnostics\Immutable;
+namespace Charcoal\App\Kernel\Diagnostics\ExecutionSnapshot\Immutable;
 
 use Charcoal\App\Kernel\Diagnostics\LogEntry;
-use Charcoal\App\Kernel\Diagnostics\LogLevel;
 use Charcoal\App\Kernel\Support\DtoHelper;
 
 /**
  * Class LogEntrySnapshot
  * @package Charcoal\App\Kernel\Diagnostics\ExecutionSnapshot\Immutable
  */
-final readonly class LogEntryImmutable
+final readonly class LogEntryDto
 {
-    public LogLevel $level;
+    public string $level;
     public string $message;
-    public int $timestamp;
+    public float $timestamp;
     public ?array $context;
     public ?array $exception;
 
     public function __construct(LogEntry $entry)
     {
-        $this->level = $entry->level;
+        $this->level = $entry->level->name;
         $this->message = $entry->message;
+        $this->timestamp = (float)$entry->timestamp->format("U.u");
+
         $this->context = $entry->context ?
             DtoHelper::createFrom($entry->context, 3, true, true, "**RECURSION**") : null;
         $this->exception = $entry->exception ?
-            DtoHelper::createFrom($entry->exception, 3, true, true, "**RECURSION**") : null;
-        $this->timestamp = $entry->timestamp->getTimestamp();
+            DtoHelper::getExceptionObject($entry->exception) : null;
     }
 }
