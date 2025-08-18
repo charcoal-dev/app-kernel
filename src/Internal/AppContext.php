3bab932fb87c11a9e0ec6710020b558d4e7bb24b
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-18 12:48:14 +0500

    Update `AppContext` constructor to include `AppEnv` and domain bindings

--- src/Internal/AppContext.php
+++ src/Internal/AppContext.php
@@ -8,16 +8,23 @@ declare(strict_types=1);
 
 namespace Charcoal\App\Kernel\Internal;
 
+use Charcoal\App\Kernel\Contracts\Domain\AppBindableInterface;
+
 /**
  * Class AppContext
  * @package Charcoal\App\Kernel\Internal
  */
 final readonly class AppContext
 {
+    /**
+     * @param \DateTimeImmutable $timestamp
+     * @param AppEnv $env
+     * @param array<string, class-string<AppBindableInterface>> $domain
+     */
     public function __construct(
-        public \DateTimeImmutable       $timestamp,
-        public array                    $moduleClasses,
-        public array                    $moduleProperties,
+        public AppEnv             $env,
+        public array              $domain,
+        public \DateTimeImmutable $timestamp,
     )
     {
     }
