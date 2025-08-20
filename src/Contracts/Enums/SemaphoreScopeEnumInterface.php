9470dbb90606fb0385a953367ab0c5118dd45903
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-20 07:28:44 +0500

    Add `SemaphoreScopeEnumInterface` extending `ConfigEnumInterface`

---
+++ src/Contracts/Enums/SemaphoreScopeEnumInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of the "charcoal-dev/app-kernel" package.
+ * @link https://github.com/charcoal-dev/app-kernel
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\App\Kernel\Contracts\Enums;
+
+use Charcoal\App\Kernel\Internal\Config\ConfigEnumInterface;
+
+/**
+ * Interface SemaphoreScopeEnumInterface
+ * @package Charcoal\App\Kernel\Contracts\Enums
+ */
+interface SemaphoreScopeEnumInterface extends ConfigEnumInterface
+{
+}
