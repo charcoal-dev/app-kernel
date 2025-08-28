a4a5a50b2bd81086d0d2e3916561e7a8255627ce
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-28 06:57:37 +0500

    Add `SemaphoreType` enum to represent semaphore mechanism types

---
+++ src/Enums/SemaphoreType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the "charcoal-dev/app-kernel" package.
+ * @link https://github.com/charcoal-dev/app-kernel
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\App\Kernel\Enums;
+
+/**
+ * Represents the different types of semaphore mechanisms that can be used.
+ */
+enum SemaphoreType
+{
+    case Filesystem;
+}
