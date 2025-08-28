1ce061f4f39af247410b7531af686e3ba4a42b52
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-28 07:12:59 +0500

    Mark `ConfigSnapshotInterface` as internal and improve its documentation for clarity

--- src/Internal/Config/ConfigSnapshotInterface.php
+++ src/Internal/Config/ConfigSnapshotInterface.php
@@ -10,7 +10,9 @@ namespace Charcoal\App\Kernel\Internal\Config;
 
 /**
  * Interface ConfigSnapshotInterface
- * @package Charcoal\App\Kernel\Internal\Config
+ * Represents a snapshot of configuration settings, providing a mechanism to interact with
+ * and retrieve configuration data within a specific state or context.
+ * @internal
  */
 interface ConfigSnapshotInterface
 {
