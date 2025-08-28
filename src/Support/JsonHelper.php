0f58a05b5a91744007d7f44f2fd0f17d59b0a641
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-28 21:25:23 +0500

    Add `JsonHelper` for processing and decoding JSON with support for imports and nested structures

---
+++ src/Support/JsonHelper.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * Part of the "charcoal-dev/app-kernel" package.
+ * @link https://github.com/charcoal-dev/app-kernel
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\App\Kernel\Support;
+
+use Charcoal\Base\Support\Helpers\DtoHelper;
+use Charcoal\Filesystem\Exceptions\FilesystemException;
+use Charcoal\Filesystem\Exceptions\PathNotFoundException;
+use Charcoal\Filesystem\Exceptions\PathTypeException;
+use Charcoal\Filesystem\Exceptions\PermissionException;
+use Charcoal\Filesystem\Node\FileNode;
+use Charcoal\Filesystem\Path\DirectoryPath;
+use Charcoal\Filesystem\Path\FilePath;
+
+/**
+ * A helper class for processing and decoding JSON data with support for importing nested structures
+ * from directories and various safety checks.
+ * @api
+ */
+abstract readonly class JsonHelper
+{
+    /**
+     * @throws FilesystemException
+     * @throws \JsonException
+     * @api
+     */
+    public static function jsonDecodeImports(
+        DirectoryPath $directory,
+        string        $jsonFile,
+
+    ): mixed
+    {
+        $seed = self::decodeJsonFromDirectory($directory, $jsonFile);
+        if (is_scalar($seed) || is_null($seed)) {
+            return $seed;
+        }
+
+        $dto = DtoHelper::createFrom($seed);
+        if (!$dto) {
+            return $dto;
+        }
+
+        return self::scan($seed, $dto, $directory);
+    }
+
+    /**
+     * @throws FilesystemException
+     * @throws \JsonException
+     */
+    private static function scan(object|array $depth, array $dto, DirectoryPath $directory): array
+    {
+        $scanned = [];
+        foreach (array_keys($dto) as $key) {
+            $vo = is_object($depth) ? $depth->{$key} : $depth[$key];
+            if (is_scalar($vo) || is_null($vo)) {
+                $scanned[$key] = $vo;
+                continue;
+            }
+
+            if (is_array($vo)) {
+                for ($i = 0; $i < count($vo); $i++) {
+                    if (is_scalar($vo[$i]) || is_null($vo[$i])) {
+                        $scanned[$key][] = $vo[$i];
+                        continue;
+                    }
+
+                    if (is_array($vo[$i]) || is_object($vo[$i])) {
+                        $scanned[$key][] = self::scan($vo[$i], $dto[$key][$i], $directory);
+                    }
+                }
+
+                continue;
+            }
+
+            if (is_object($vo)) {
+                $buffer = null;
+                //if (in_array("\$imports", array_keys($dto[$key]), true)) {
+                if (array_key_exists("\$imports", $dto[$key])) {
+                    $importer = $vo;
+                    $baggage = self::jsonImporter($directory, $importer);
+                    if ($baggage) {
+                        if (empty((array)$importer)) {
+                            $scanned[$key] = $baggage;
+                            continue;
+                        }
+                    }
+
+                    $dto[$key] = DtoHelper::createFrom($importer);
+                }
+
+                if ($buffer === null) {
+                    $buffer = $vo;
+                }
+
+                foreach (self::scan($buffer, $dto[$key], $directory) as $ck => $cv) {
+                    $scanned[$key][$ck] = $cv;
+                }
+            }
+        }
+
+        return $scanned;
+    }
+
+    /**
+     * @throws FilesystemException
+     * @throws \JsonException
+     */
+    private static function jsonImporter(DirectoryPath $directory, object $importer): array|object
+    {
+        $imports = $importer->{"\$imports"};
+        if (!is_array($imports) || !$imports) {
+            return [];
+        }
+
+        $buffer = [];
+        foreach ($imports as $import) {
+            $buffer[] = self::decodeJsonFromDirectory($directory, $import);
+        }
+
+        unset($importer->{"\$imports"});
+        $baggage = [];
+        foreach ($buffer as $imported) {
+            if (is_scalar($imported) || is_null($imported)) {
+                $baggage[] = $imported;
+                continue;
+            }
+
+            if (is_array($imported)) {
+                if (array_is_list($imported)) {
+                    $baggage = [...$baggage, ...$imported];
+                } else {
+                    foreach (array_keys(DtoHelper::createFrom((object)$imported, 1)) as $item) {
+                        $importer->{$item} = $imported[$item];
+                    }
+                }
+                continue;
+            }
+
+            if (is_object($imported)) {
+                // unset($imported->{"\$schema"});
+                foreach (array_keys(DtoHelper::createFrom($imported, 1)) as $item) {
+                    $importer->{$item} = $imported->{$item};
+                }
+            }
+        }
+
+        return $baggage;
+    }
+
+    /**
+     * @throws PermissionException
+     * @throws FilesystemException
+     * @throws \JsonException
+     */
+    public static function decodeJsonFromDirectory(DirectoryPath $directory, string $filename): mixed
+    {
+        return self::decodeJsonFromFile($directory->join($filename . ".json")->isFile());
+    }
+
+    /**
+     * @throws PermissionException
+     * @throws PathNotFoundException
+     * @throws PathTypeException
+     * @throws FilesystemException
+     * @throws \JsonException
+     */
+    public static function decodeJsonFromFile(FileNode|FilePath $path): mixed
+    {
+        return json_decode(trim(match (true) {
+            $path instanceof FileNode => $path->read(),
+            default => $path->node()->read()
+        }), false, flags: JSON_THROW_ON_ERROR);
+    }
+}
