ff5cc02eb716124eef1f41ccb6f7c98446523861
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-30 02:21:26 +0500

    Simplify `AppCrashException` usage in `ErrorManager` and clarify internal exception handling documentation.

--- src/Internal/Exceptions/AppCrashException.php
+++ src/Internal/Exceptions/AppCrashException.php
@@ -11,7 +11,7 @@ namespace Charcoal\App\Kernel\Internal\Exceptions;
 /**
  * Represents an exception thrown when an application crashes or encounters a critical failure.
  * Extends the base Exception class to provide specific handling for application-level crashes.
- * @internal Do NOT catch!
+ * @internal Do NOT catch except on final error boundary!
  */
 final class AppCrashException extends \RuntimeException
 {
