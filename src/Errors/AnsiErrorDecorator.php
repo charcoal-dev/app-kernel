906167b323df80335b47f64a3c0358232097a12e
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-31 05:50:32 +0500

    Fix formatting of trace color in `AnsiErrorDecorator` template for improved consistency.

--- src/Errors/AnsiErrorDecorator.php
+++ src/Errors/AnsiErrorDecorator.php
@@ -106,6 +106,6 @@ abstract class AnsiErrorDecorator implements ErrorLoggerInterface
             // [...]: Error DTO template:
             "{red}[@{:class:}][{yellow}#@{:code:}{red}]{/}",
             "@{:message:}",
-            "{yellow}Trace:{/}\x20{blue}[@{:file:} {yellow}#{:line:}{blue}]{/}"];
+            "{yellow}Trace:{/}\x20{blue}[@{:file:} {yellow}#{yellow}{:line:}{blue}]{/}"];
     }
 }
