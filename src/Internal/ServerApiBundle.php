c9d6550bcafc5297e0cbd130b6710168dc1579f3
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-01 06:59:11 +0500

    Rename `AppRoutesBundle` to `ServerApiBundle` and update type annotations to align with `ServerApi` terminology.

--- src/Internal/ServerApiBundle.php
+++ src/Internal/ServerApiBundle.php
@@ -8,20 +8,20 @@ declare(strict_types=1);
 
 namespace Charcoal\App\Kernel\Internal;
 
-use Charcoal\App\Kernel\Contracts\EntryPoint\AppRoutesProviderInterface;
 use Charcoal\Base\Vectors\AbstractVector;
+use Charcoal\Contracts\ServerApi\ServerApiInterface;
 
 /**
  * Represents a collection of AppRoutes objects.
  * This class is a bundle that extends the AbstractVector class and
  * provides functionality to manage multiple AppRoutes objects.
- * @extends AbstractVector<AppRoutesProviderInterface>
- * @method AppRoutesProviderInterface[] getIterator()
- * @method AppRoutesProviderInterface[] getArray()
+ * @extends AbstractVector<ServerApiInterface>
+ * @method ServerApiInterface[] getIterator()
+ * @method ServerApiInterface[] getArray()
  */
-final class AppRoutesBundle extends AbstractVector
+final class ServerApiBundle extends AbstractVector
 {
-    public function __construct(AppRoutesProviderInterface ...$routes)
+    public function __construct(ServerApiInterface ...$routes)
     {
         parent::__construct($routes);
     }
